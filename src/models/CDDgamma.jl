ceb186dfe2c252cfe4239e94bc9f539a41468419
Arthur Lui <luiarthur@gmail.com>
2020-12-04 23:53:53 -0700

    bug fix

--- src/models/CDDgamma.jl
+++ src/models/CDDgamma.jl
@@ -10,9 +10,10 @@ function CDDg0(NC::Int, NT::Int, QC::Int, QT::Int, nsamps::Int; a::Real=1, b::Re
   dist = Beta(a + Qsum, b + Nsum - Qsum)
 
   # Posterior samples of gammaC (and gammaT)
-  gammaC_samples = rand(dist, nsamps)
+  gamma_samples = rand(dist, nsamps)
 
-  return (gammaC_samples=gammaC_samples, gammaT_samples=gammaC_samples,
+  return (gammaC_samples=gamma_samples,
+          gammaT_samples=gamma_samples,
           distC=dist)
 end
 
@@ -21,9 +22,6 @@ Model1: Proportion of zeros are different.
 Return posterior samples of gammaC and gammaT.
 """
 function CDDg1(NC::Int, NT::Int, QC::Int, QT::Int, nsamps::Int; a::Real=1, b::Real=1)
-  Nsum = NC + NT
-  Qsum = QC + QT
-  
   # Posterior distributions.
   distC = Beta(a + QC, b + NC - QC)
   distT = Beta(a + QT, b + NT - QT)
@@ -32,6 +30,8 @@ function CDDg1(NC::Int, NT::Int, QC::Int, QT::Int, nsamps::Int; a::Real=1, b::Re
   gammaC_samples = rand(distC, nsamps)
   gammaT_samples = rand(distT, nsamps)
 
-  return (gammaC_samples=gammaC_samples, gammaT_samples=gammaC_samples,
-          distC=distC, distT=distT)
+  return (gammaC_samples=gammaC_samples,
+          gammaT_samples=gammaT_samples,
+          distC=distC,
+          distT=distT)
 end
