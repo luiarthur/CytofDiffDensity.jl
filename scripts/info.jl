c58541557f56f880785f1a5d0f2eb1b2e67e5764
Arthur Lui <luiarthur@gmail.com>
2020-12-06 20:18:46 -0700

    moved info to julia file

---
+++ scripts/info.jl
@@ -0,0 +1,14 @@
+module Info
+
+project_name = "cytof-diff-dens"
+
+awsbucket = "s3://$(project_name)"
+resultsdir = joinpath(ENV["SCRATCH_DIR"], project_name)
+
+awsbucket_datarun = joinpath(awsbucket, "datarun")
+resultsdir_datarun = joinpath(resultsdir, "datarun")
+
+awsbucket_simstudy = joinpath(awsbucket, "simstudy")
+resultsdir_simstudy = joinpath(resultsdir, "simstudy")
+
+end  # module
--- src/CytofDiffDensity.jl
+++ src/CytofDiffDensity.jl
@@ -1,13 +1,10 @@
 module CytofDiffDensity
 
-using YAML
 using MCMC
 using Distributions
 using StatsFuns
 using Random
 using MCMC: Bijectors
-
-export load_and_sanitize_yaml
 
 export CDDG, CDDgamma, infer
 export MixSkewT
@@ -17,7 +14,6 @@ export AltSkewT, skewfromaltskewt, scalefromaltskewt, fromaltskewt, toaltscale, 
 export make_sampler
 export loglike_G, loglike_gamma
 
-include("util.jl")
 include("skewt.jl")
 include("models/CDDG.jl")
 include("models/CDDG_updates.jl")
