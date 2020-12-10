9fec452f473b76535a080207b7ce0bd94e096fa2
Arthur Lui <luiarthur@gmail.com>
2020-12-10 10:49:40 -0700

    Update OrderedNormalMeanPrior.jl

--- test/OrderedNormalMeanPrior.jl
+++ test/OrderedNormalMeanPrior.jl
@@ -10,12 +10,12 @@ println("Test OrderedNormalMeanPrior")
     onm_prior = OrderedNormalMeanPrior(K, Normal(0, 6), truncated(Normal(1, .3), 0, Inf))
     model = MixSkewT(y, K, mu=onm_prior)
     cdd.print_model_info(model)
-    spl = make_sampler(model, tdist=false, skew= false)
+    spl = make_sampler(model, tdist=false, skew=false)
 
     nsamps = 1000
     chain, metrics = mcmc(spl, nsamps, nburn=2000, thin=1)
     @test length(unique(chain)) == nsamps
-    @test isapprox(mean.(mm.components), mean(getindex.(chain, :mu)), atol=1e-1)
-    @test isapprox(std.(mm.components), mean(getindex.(chain, :sigma)), atol=1e-1)
+    @test isapprox(mean.(mm.components), mean(getindex.(chain, :mu)), atol=0.2)
+    @test isapprox(std.(mm.components), mean(getindex.(chain, :sigma)), atol=0.1)
   end
 end
