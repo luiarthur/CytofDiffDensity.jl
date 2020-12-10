4dec9b8b977f66e2b666c016cb6026fc45678ad5
Arthur Lui <luiarthur@gmail.com>
2020-12-10 10:32:36 -0700

    Update OrderedNormalMeanPrior.jl

--- test/OrderedNormalMeanPrior.jl
+++ test/OrderedNormalMeanPrior.jl
@@ -2,6 +2,7 @@ println("Test OrderedNormalMeanPrior")
 
 @testset "OrderedNormalMeanPrior" begin
   @testset "update" begin
+    Random.seed!(0)
     K = 3
     mm = MixtureModel([Normal(-2, .3), Normal(0, .2), Normal(2, .5)], [.3, .4, .3])
     y = rand(mm, 500)
@@ -9,7 +10,7 @@ println("Test OrderedNormalMeanPrior")
     onm_prior = OrderedNormalMeanPrior(K, Normal(0, 6), truncated(Normal(1, .3), 0, Inf))
     model = MixSkewT(y, K, mu=onm_prior)
     cdd.print_model_info(model)
-    spl = make_sampler(model)
+    spl = make_sampler(model, tdist=false, skew= false)
 
     nsamps = 1000
     chain, metrics = mcmc(spl, nsamps, nburn=2000, thin=1)
