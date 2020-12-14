c31462da157b90b81dc46d446290e585df246073
Arthur Lui <luiarthur@gmail.com>
2020-12-14 16:31:47 -0700

    updated simulation

--- scripts/simstudy/v1/scenarios.jl
+++ scripts/simstudy/v1/scenarios.jl
@@ -4,50 +4,50 @@ function scenarios(n::Int; seed=nothing, Ni=1000)
   if n == 0  # TEST CASE!
     return scenarios(1, seed=seed, Ni=100)
   elseif n == 1
-    # (gammaC=0.1, gammaT=0.1)
-    return simulate_gtilde(NC=Ni, NT=Ni,
+    return simulate_ftilde(NC=Ni, NT=Ni,
                            etaC=[.25, .75, 0, 0],
                            etaT=[.05, .05, .9, 0], 
                            loc = [-1.5, 3.5, 5.1, 5],
                            scale = [1.6, 1.76, 1.76, 1.6],
                            df = [12, 10, 10, 15.],
-                           skew = [0, -10, -10, 0.])
+                           skew = [0, -10, -10, 0.],
+                           gammaC=0.1, gammaT=0.1)
   elseif n == 2
-    # (gammaC=0.1, gammaT=0.1)
-    return simulate_gtilde(NC=Ni, NT=Ni,
+    return simulate_ftilde(NC=Ni, NT=Ni,
                            etaC=[.05, .05, .5, .4], 
                            etaT=[.05, .05, .9, 0], 
                            loc = [-1.5, 3.5, 5.1, 4.3],
                            scale = [1.6, 1.76, 1.76, 1.6],
                            df = [12, 10, 10, 15.],
-                           skew = [0, -10, -10, -11.])
+                           skew = [0, -10, -10, -11.],
+                           gammaC=0.1, gammaT=0.1)
   elseif n == 3
-    # (gammaC=0.1, gammaT=0.05)
-    return simulate_gtilde(NC=Ni, NT=Ni,
+    return simulate_ftilde(NC=Ni, NT=Ni,
                            etaC=[.05, .05, .5, .4], 
                            etaT=[.05, .05, .5, .4], 
                            loc = [-1.5, 3.5, 5.1, 4.3],
                            scale = [1.6, 1.76, 1.76, 1.6],
                            df = [12, 10, 10, 15.],
-                           skew = [0, -10, -10, -11.])
+                           skew = [0, -10, -10, -11.],
+                           gammaC=0.1, gammaT=0.05)
   elseif n == 4
-    # (gammaC=0.1, gammaT=0.1)
-    return simulate_gtilde(NC=Ni, NT=Ni,
+    return simulate_ftilde(NC=Ni, NT=Ni,
                            etaC=[.05, .05, .5, .4], 
                            etaT=[.05, .05, .5, .4], 
                            loc = [-1.5, 3.5, 5.1, 4.3],
                            scale = [1.6, 1.76, 1.76, 1.6],
                            df = [12, 10, 10, 15.],
-                           skew = [0, -10, -10, -11.])
+                           skew = [0, -10, -10, -11.],
+                           gammaC=0.1, gammaT=0.1)
   elseif n == 5  # a test case
-    # (gammaC=0.1, gammaT=0.1)
-    return simulate_gtilde(NC=Ni, NT=Ni,
+    return simulate_ftilde(NC=Ni, NT=Ni,
                            etaC=[0, 1., 0, 0],
                            etaT=[0, 0, 1., 0],
                            loc = [-1.5, 3.5, 5.1, 4.3],
                            scale = [1.6, 1.76, 1.76, 1.6],
                            df = [12, 10, 10, 15.],
-                           skew = [0, -10, -10, -11.])
+                           skew = [0, -10, -10, -11.],
+                           gammaC=0.1, gammaT=0.1)
   else
     throw(ArgumentError("n=$(n) was provided. But n must be an integer between 0 and 5."))
   end
