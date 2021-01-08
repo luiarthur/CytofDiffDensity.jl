1fb423d9903967af15300510cd913694422cefbc
Arthur Lui <luiarthur@gmail.com>
2021-01-07 18:13:25 -0800

    fixed slurm issues

--- scripts/datastudy/v3/slurm-spawn-jobs.sh
+++ scripts/datastudy/v3/slurm-spawn-jobs.sh
@@ -7,10 +7,9 @@
 #SBATCH --mail-user=alui2@ucsc.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o out/slurm-job.out    # Name of stdout output file
-#SBATCH -N 3  # number of nodes
-#SBATCH --ntasks-per-node=24  # maximum number of tasks to be run on a node.
+#SBATCH --nodes=3 --ntasks-per-node=24 --cpus-per-task=1
 #SBATCH -t 120:00:00  # Run Time (hh:mm:ss)
-#SBATCH --mem=42G # Memory to be allocated PER NODE
+#SBATCH --mem-per-cpu=2G
 
 date 
 echo "nproc: `nproc`"
@@ -25,7 +24,7 @@ Ks=`seq 2 9`
 skewmixt="0 1"
 
 echo "Compiling job ..."
-srun -N1 -n1 -c1 julia imports.jl
+srun -n1 julia imports.jl
 
 echo "Starting jobs!"
 for marker in $markers
@@ -35,7 +34,8 @@ do
     for stm in $skewmixt
     do
       logname="marker=${marker}_K=${K}_stm=${stm}"
-      srun -N1 -n1 -c1 julia run-single.jl $marker $K $stm &> out/${logname}-log.txt &
+      cmd="julia run-single.jl $marker $K $stm"
+      srun -n1 $cmd &> out/${logname}-log.txt &
     done
   done
 done
